9781e72cda8a99f2ebf533be46630351a83f9ba5
Korvis77 <korvis777@gmail.com>
2026-04-02 11:12:41 -0500

    Construction for 2x2 and 1 node triangle units

--- init.go
+++ init.go
@@ -28,35 +28,6 @@ type NodeVector struct {
 	ysummedforces float32
 }
 
-func ConstructUnitTemplates() {
-
-	//First Vector at 9 o'clock
-	unitType := "2x2"
-	Construct2x2UnitTemplate(unitType)
-
-	//repeat above pattern
-}
-
-func Construct2x2UnitTemplate(unitTypePassed string) UnitTemplate {
-
-	nodeVector1 := NodeVector{[]int{1, 4}, -2.5, -2.5, 0, 0}
-
-	node1 := ConstructNodeCore(1, 0, 0, []NodeVector{nodeVector1})
-	// node2 := ConstructNodeCore(2, 5, 0, []int{3, 4, 5})
-	// node3 := ConstructNodeCore(3, 5, 5, []int{5, 6, 7})
-	// node4 := ConstructNodeCore(4, 0, 5, []int{7, 8, 1})
-
-	//constructedNodeSlice := []NodeCore{node1}
-
-	unit := UnitTemplate{
-		unitType:  "2x2",
-		xStarting: 0,
-		yStarting: 0,
-		nodeSlice: []NodeCore{node1}}
-
-	return unit
-}
-
 func ConstructNodeCore(
 	nodeID int,
 	x float32,
@@ -66,4 +37,58 @@ func ConstructNodeCore(
 	node := NodeCore{nodeID, x, y, 0, 0, childVectors}
 
 	return node
+}
+
+func ConstructUnitTemplates() {
+
+
+	Construct2x2UnitTemplate("2x2")
+	Construct1NodeTriangleUnitTemplate("1NodeTriangle")
+
+}
+
+func Construct2x2UnitTemplate(unitTypePassed string) UnitTemplate {
+
+	//First Vector at 9 o'clock, everything goes clockwise starting at 9
+	//Offset is based off 1st vector in list FOR NOW
+	nodeVector1 := NodeVector{[]int{1, 4}, -2.5, -2.5, 0, 0}
+	nodeVector2 := NodeVector{[]int{1}, -2.5, 2.5, 0, 0}
+	nodeVector3 := NodeVector{[]int{1, 2}, 2.5, 2.5, 0, 0}
+	nodeVector4 := NodeVector{[]int{2}, 2.5, 2.5, 0, 0}
+	nodeVector5 := NodeVector{[]int{2, 3}, 2.5, -2.5, 0, 0}
+	nodeVector6 := NodeVector{[]int{3}, 2.5, 2.5, 0, 0}
+	nodeVector7 := NodeVector{[]int{3, 4}, -2.5, 2.5, 0, 0}
+	nodeVector8 := NodeVector{[]int{4}, -2.5, 2.5, 0, 0}
+
+	node1 := ConstructNodeCore(1, 0, 0, []NodeVector{nodeVector1, nodeVector2, nodeVector3})
+	node2 := ConstructNodeCore(2, 0, 0, []NodeVector{nodeVector3, nodeVector4, nodeVector5})
+	node3 := ConstructNodeCore(3, 0, 0, []NodeVector{nodeVector5, nodeVector6, nodeVector7})
+	node4 := ConstructNodeCore(4, 0, 0, []NodeVector{nodeVector7, nodeVector8, nodeVector1})
+
+	unit := UnitTemplate{
+		unitType:  "2x2",
+		xStarting: 0,
+		yStarting: 0,
+		nodeSlice: []NodeCore{node1, node2, node3, node4}}
+
+	return unit
+}
+
+
+func Construct1NodeTriangleUnitTemplate(unitTypePassed string) UnitTemplate {
+
+	//First Vector at 9 o'clock, everything goes clockwise starting at 9
+	//Offset is based off 1st vector in list FOR NOW
+	nodeVector1 := NodeVector{[]int{1}, -3, -1.5, 0, 0}
+	nodeVector2 := NodeVector{[]int{1}, 3, 0, 0, 0}
+	nodeVector3 := NodeVector{[]int{1}, -3, 1.5, 0, 0}
+
+	node1 := ConstructNodeCore(1, 0, 0, []NodeVector{nodeVector1, nodeVector2, nodeVector3})
+	unit := UnitTemplate{
+		unitType:  "1NodeTriangle",
+		xStarting: 0,
+		yStarting: 0,
+		nodeSlice: []NodeCore{node1}
+
+	return unit
 }
