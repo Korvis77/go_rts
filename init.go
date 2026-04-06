01ee713d38e2b18d9c852b0bef4e126a4a7b2263
Dorime Dog <korvis777@gmail.com>
2026-04-05 20:11:59 -0500

    DrawUnit() --> need to re-think x and y Cores vs Boundaries

--- init.go
+++ init.go
@@ -25,6 +25,7 @@ type CoreNode struct {
 
 type VectorNode struct {
 	vectorID        int
+	parentCores     [][]int32
 	parentCoreSlice []int
 	xOffset         int32
 	yOffset         int32
@@ -34,52 +35,56 @@ type VectorNode struct {
 func Build2x2UnitTemplate() *UnitTemplateStruct {
 
 	vector1 := VectorNode{
-		vectorID:        1,
-		parentCoreSlice: []int{1, 4},
-		xOffset:         -3,
-		yOffset:         3,
+		vectorID: 1,
+		parentCores: [][]int32{
+			{1, -3, 3},
+			{4, 3, 3},
+		},
 	}
 	vector2 := VectorNode{
-		vectorID:        2,
-		parentCoreSlice: []int{1},
-		xOffset:         -3,
-		yOffset:         -3,
+		vectorID: 2,
+		parentCores: [][]int32{
+			{1, -3, -3},
+		},
 	}
 	vector3 := VectorNode{
-		vectorID:        3,
-		parentCoreSlice: []int{1, 2},
-		xOffset:         3,
-		yOffset:         -3,
+		vectorID: 3,
+		parentCores: [][]int32{
+			{1, 3, -3},
+			{2, -3, -3},
+		},
 	}
 	vector4 := VectorNode{
-		vectorID:        4,
-		parentCoreSlice: []int{2},
-		xOffset:         3,
-		yOffset:         -3,
+		vectorID: 4,
+		parentCores: [][]int32{
+			{2, 3, -3},
+		},
 	}
 	vector5 := VectorNode{
-		vectorID:        5,
-		parentCoreSlice: []int{2, 3},
-		xOffset:         3,
-		yOffset:         3,
+		vectorID: 5,
+		parentCores: [][]int32{
+			{2, 3, 3},
+			{3, 3, -3},
+		},
 	}
 	vector6 := VectorNode{
-		vectorID:        6,
-		parentCoreSlice: []int{3},
-		xOffset:         3,
-		yOffset:         3,
+		vectorID: 6,
+		parentCores: [][]int32{
+			{3, 3, 3},
+		},
 	}
 	vector7 := VectorNode{
-		vectorID:        7,
-		parentCoreSlice: []int{3, 4},
-		xOffset:         -3,
-		yOffset:         3,
+		vectorID: 7,
+		parentCores: [][]int32{
+			{3, -3, 3},
+			{4, -3, -3},
+		},
 	}
 	vector8 := VectorNode{
-		vectorID:        8,
-		parentCoreSlice: []int{4},
-		xOffset:         -3,
-		yOffset:         3,
+		vectorID: 8,
+		parentCores: [][]int32{
+			{4, -3, 3},
+		},
 	}
 
 	unitTemplate := &UnitTemplateStruct{
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	_ "image/color"
 
@@ -16,7 +17,8 @@ func main() {
 	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 
-	NodeColor := color.RGBA{0, 0, 0, 255}
+	unitBoundaryColor := color.RGBA{0, 0, 0, 255}
+	boundaryNodeColor := color.RGBA{0, 255, 0, 255}
 	var nodeTestText string = "testing testing"
 
 	// StoreUserInputMap := map[string]bool{
@@ -25,6 +27,7 @@ func main() {
 	// }
 
 	UnitTemplate2x2 := Build2x2UnitTemplate()
+	fmt.Printf("vector1 nested slices: %d  ", UnitTemplate2x2.vectorNodeSlice[0])
 
 	rl.SetTargetFPS(60)
 
@@ -38,9 +41,17 @@ func main() {
 		rl.DrawText(nodeTestText, 190, 200, 20, rl.LightGray)
 		//rl.DrawCircle(300, 300, 300, NodeColor)
 
-		DrawUnitOutline(UnitTemplate2x2, 300, 300, NodeColor)
+		DrawUnit(UnitTemplate2x2, 300, 300, unitBoundaryColor, boundaryNodeColor)
 
 		rl.EndDrawing()
+	}
+}
+
+func ActiveTesting() {
+	testlen := [][]int32{{1, 3, -3}, {2, -3, -3}}
+	for index, value := range testlen {
+		fmt.Printf("Index: %d  wow cool \n ", index)
+		fmt.Printf("Value: %d  wow cool \n ", value)
 	}
 }
 
@@ -58,29 +69,105 @@ func DrawMousePath(v1 rl.Vector2, v2 rl.Vector2, color color.RGBA) {
 	rl.DrawLine(v1x, v1y, v2x, v2y, color)
 }
 
-func DrawUnitOutline(unitTemplate *UnitTemplateStruct, xtransform int32, ytransform int32, nodeColor color.RGBA) {
-	vectorCount := len(unitTemplate.coreNodeSlice)
+func DrawUnit(
+	unitTemplate *UnitTemplateStruct,
+	xtransform int32,
+	ytransform int32,
+	unitBoundaryColor color.RGBA,
+	coreNodeColor color.RGBA,
+) {
 
-	for i := 0; i < vectorCount; i++ {
+	var coreNode1X int32
+	var coreNode1Y int32
+	var coreNode2X int32
+	var coreNode2Y int32
 
-		j := i + 1
+	var sumBoundaryNodeOffsetX int32
+	var sumBoundaryNodeOffsetY int32
 
-		//Iterate on building position after applying sum of forces:
+	var boundaryNode1X int32
+	var boundaryNode1Y int32
+	// var boundaryNode2X int32
+	// var boundaryNode2Y int32
+	// var sumPrevBoundaryNodeOffsetX int32
+	// var sumPrevBoundaryNodeOffsetY int32
+	// var offsetCore1X int32
+	// var offsetCore1Y int32
+	// var offsetCore2X int32
+	// var offsetCore2Y int32
 
-		if i == vectorCount-1 {
-			vec1xpos := unitTemplate.coreNodeSlice[0].xOffset + xtransform
-			vec1ypos := unitTemplate.coreNodeSlice[0].yOffset + ytransform
-			vec2xpos := unitTemplate.coreNodeSlice[i].xOffset + xtransform
-			vec2ypos := unitTemplate.coreNodeSlice[i].yOffset + ytransform
-			rl.DrawLine(vec1xpos, vec1ypos, vec2xpos, vec2ypos, nodeColor)
+	coreNodeCount := len(unitTemplate.coreNodeSlice)
+	for index, _ := range unitTemplate.coreNodeSlice {
+		if index == coreNodeCount-1 {
+			coreNode1X = unitTemplate.coreNodeSlice[0].xOffset + xtransform
+			coreNode1Y = unitTemplate.coreNodeSlice[0].yOffset + ytransform
+			coreNode2X = unitTemplate.coreNodeSlice[index].xOffset + xtransform
+			coreNode2Y = unitTemplate.coreNodeSlice[index].yOffset + ytransform
+			rl.DrawCircle(coreNode2X, coreNode2Y, 5, coreNodeColor)
 		} else {
-			vec1xpos := unitTemplate.coreNodeSlice[i].xOffset + xtransform
-			vec1ypos := unitTemplate.coreNodeSlice[i].yOffset + ytransform
-			vec2xpos := unitTemplate.coreNodeSlice[j].xOffset + xtransform
-			vec2ypos := unitTemplate.coreNodeSlice[j].yOffset + ytransform
-			rl.DrawLine(vec1xpos, vec1ypos, vec2xpos, vec2ypos, nodeColor)
+			coreNode1X = unitTemplate.coreNodeSlice[index].xOffset + xtransform
+			coreNode1Y = unitTemplate.coreNodeSlice[index].yOffset + ytransform
+			// coreNode2X = unitTemplate.coreNodeSlice[index+1].xOffset + xtransform
+			// coreNode2Y = unitTemplate.coreNodeSlice[index+1].yOffset + ytransform
 		}
+		rl.DrawCircle(coreNode1X, coreNode1Y, 5, coreNodeColor)
 	}
+
+	for _, value := range unitTemplate.vectorNodeSlice {
+
+		parentList := value.parentCores
+		for _, value2 := range parentList {
+			sumBoundaryNodeOffsetX += value2[1]
+			sumBoundaryNodeOffsetY += value2[2]
+		}
+
+		boundaryNode1X = sumBoundaryNodeOffsetX + xtransform
+		boundaryNode1Y = sumBoundaryNodeOffsetY + ytransform
+		// fmt.Printf("SumNodeX: %d  wow cool ", sumBoundaryNodeOffsetX)
+		// fmt.Printf("SumNodeY: %d  wow cool \n ", sumBoundaryNodeOffsetY)
+		rl.DrawCircle(boundaryNode1X, boundaryNode1Y, 3, unitBoundaryColor)
+	}
+
+	// Draw Node Cores
+	// coreNodeCount := len(unitTemplate.coreNodeSlice)
+	// for i := 0; i < coreNodeCount; i++ {
+	// 	j := i + 1
+
+	// 	if i == coreNodeCount-1 {
+	// 		vec1xpos = unitTemplate.coreNodeSlice[0].xOffset + xtransform
+	// 		vec1ypos = unitTemplate.coreNodeSlice[0].yOffset + ytransform
+	// 		vec2xpos = unitTemplate.coreNodeSlice[i].xOffset + xtransform
+	// 		vec2ypos = unitTemplate.coreNodeSlice[i].yOffset + ytransform
+	// 	} else {
+	// 		vec1xpos = unitTemplate.coreNodeSlice[i].xOffset + xtransform
+	// 		vec1ypos = unitTemplate.coreNodeSlice[i].yOffset + ytransform
+	// 		vec2xpos = unitTemplate.coreNodeSlice[j].xOffset + xtransform
+	// 		vec2ypos = unitTemplate.coreNodeSlice[j].yOffset + ytransform
+	// 	}
+	// 	//rl.DrawLine(vec1xpos, vec1ypos, vec2xpos, vec2ypos, unitBoundaryColor)
+	// 	rl.DrawCircle(vec1xpos, vec1ypos, 5, nodeCoreColor)
+	// }
+
+	// // Draw Unit Boundary
+	// boundaryNodeCount := len(unitTemplate.vectorNodeSlice)
+	// for i := 0; i < boundaryNodeCount; i++ {
+	// 	j := i + 1
+
+	// 	if i == coreNodeCount-1 {
+	// 		vec1xpos = unitTemplate.vectorNodeSlice[0].xOffset + xtransform
+	// 		vec1ypos = unitTemplate.vectorNodeSlice[0].yOffset + ytransform
+	// 		vec2xpos = unitTemplate.vectorNodeSlice[i].xOffset + xtransform
+	// 		vec2ypos = unitTemplate.coreNodeSlice[i].yOffset + ytransform
+	// 	} else {
+	// 		vec1xpos = unitTemplate.coreNodeSlice[i].xOffset + xtransform
+	// 		vec1ypos = unitTemplate.coreNodeSlice[i].yOffset + ytransform
+	// 		vec2xpos = unitTemplate.coreNodeSlice[j].xOffset + xtransform
+	// 		vec2ypos = unitTemplate.coreNodeSlice[j].yOffset + ytransform
+	// 	}
+	// 	//rl.DrawLine(vec1xpos, vec1ypos, vec2xpos, vec2ypos, unitBoundaryColor)
+	// 	rl.DrawCircle(vec1xpos, vec1ypos, 5, nodeCoreColor)
+	// }
+
 }
 
 /* vector length, this is the hypotenuse,
